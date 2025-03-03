037ebeb2a116a811219343a27878f249efefa39d
FabioBritto <fabio.tritono@gmail.com>
2025-03-03 20:45:01 -0300

    First commit

---
+++ src/test/java/com/fabiobritto/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiobritto.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
