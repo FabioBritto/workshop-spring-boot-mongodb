e6680240589c02cbd2aeb24cdb6b960db794334f
FabioBritto <fabio.tritono@gmail.com>
2025-03-05 10:39:05 -0300

    Atualização de User com PUT

--- src/main/java/com/fabiobritto/workshopmongo/services/UserService.java
+++ src/main/java/com/fabiobritto/workshopmongo/services/UserService.java
@@ -40,4 +40,15 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repository.save(newUser);
+	}
+	
+	public void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+	
 }
