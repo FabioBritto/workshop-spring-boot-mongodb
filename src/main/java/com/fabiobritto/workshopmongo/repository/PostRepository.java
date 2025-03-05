61c19ec06f495d1e930d8db36ec1b31e7f21a7ff
FabioBritto <fabio.tritono@gmail.com>
2025-03-05 13:06:57 -0300

    Consulta personalizada com Query Method

--- src/main/java/com/fabiobritto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fabiobritto/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.fabiobritto.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.fabiobritto.workshopmongo.domain.Post;
+import com.fabiobritto.workshopmongo.dto.CommentDTO;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	public List<Post> findByTitleContainingIgonoreCase(String text);
 }
--- src/main/java/com/fabiobritto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/fabiobritto/workshopmongo/resources/PostResource.java
@@ -1,14 +1,18 @@
 package com.fabiobritto.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabiobritto.workshopmongo.domain.Post;
+import com.fabiobritto.workshopmongo.resources.util.URL;
 import com.fabiobritto.workshopmongo.services.PostService;
 
 @RestController
@@ -21,8 +25,14 @@ public class PostResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post post = service.findById(id);
-		//Por algum motivo, o ID estava indo como null. Por isso, setei manualmente
-		//post.getAuthor().setId(id);
 		return ResponseEntity.status(HttpStatus.OK).body(post);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+		text = URL.decodificarParam(text);
+		List<Post> posts = service.findByTitle(text);
+		return ResponseEntity.status(HttpStatus.OK).body(posts);
+		
 	}
 }
