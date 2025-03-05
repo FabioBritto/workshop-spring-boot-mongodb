6bbd917dc41c9a3f025c79bc59348c887133793b
FabioBritto <fabio.tritono@gmail.com>
2025-03-05 09:09:35 -0300

    Obtendo usuário por ID e melhorando tratamento de erros/exceções

--- src/main/java/com/fabiobritto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fabiobritto/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,12 @@ public class UserResource {
 		List<User> users = service.findAll();
 		List<UserDTO> dtos = users.stream().map(o -> new UserDTO(o)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(dtos);		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		UserDTO dto = new UserDTO(user);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- src/main/java/com/fabiobritto/workshopmongo/services/UserService.java
+++ src/main/java/com/fabiobritto/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.fabiobritto.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fabiobritto.workshopmongo.domain.User;
 import com.fabiobritto.workshopmongo.repository.UserRepository;
+import com.fabiobritto.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user =repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));	
 	}
 }
